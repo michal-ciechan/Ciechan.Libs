70b76dd544978851da56abf57225935570059b1e
Michal Ciechan <lndcobra@hotmail.com>
2020-09-25 13:35:51 +0100

    Collections

---
+++ src/Ciechan.Libs.Collections/ListBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ciechan.Libs.Collections
+{
+    public class ListBuilder<T>
+    {
+        public bool PreferEmpty { get; set; }
+
+        public List<T> ToList()
+        {
+            if(PreferEmpty)
+                return new List<T>();
+
+            return null;
+        }
+    }
+}
---
+++ src/Ciechan.Libs.Tests/Collections/ListBuilderTests.cs
@@ -0,0 +1,32 @@
+﻿using Ciechan.Libs.Collections;
+using FluentAssertions;
+using Xunit;
+
+namespace Ciechan.Libs.Tests.Collections
+{
+    public class ListBuilderTests
+    {
+        [Fact]
+        public void Default_ShouldReturnNull()
+        {
+            var builder = new ListBuilder<int>();
+
+            var res = builder.ToList();
+
+            res.Should().BeNull();
+        }
+        
+        [Fact]
+        public void Default_PreferEmpty_ShouldReturnEmptyList()
+        {
+            var builder = new ListBuilder<int>
+            {
+                PreferEmpty = true,
+            };
+
+            var res = builder.ToList();
+
+            res.Should().BeEmpty();
+        }
+    }
+}
